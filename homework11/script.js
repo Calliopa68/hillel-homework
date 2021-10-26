b7b5e7bd1818f6bdc0fb70e8749a208dd09560e6
calliopa <calliopa@centro.team>
2021-10-26 19:01:14 +0300

    homework11

--- homework11/script.js
+++ homework11/script.js
@@ -2,12 +2,15 @@
     const loginEl = document.getElementById('login');
     const passwordEl = document.getElementById('password');
     const buttonEl = document.getElementById('button');
+    const formEl = document.getElementById('form');
+    const messageEl = document.getElementById('success-message');
 
     const validateEmailInput = (email) => {
         const reg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
         return reg.test(email);
 
     }
+    
     const showErrorMessage = () => {
         loginEl.addEventListener('focusout', () => {
             const errorMessage = document.createElement('span');
@@ -19,6 +22,31 @@
         }, { once: true });
     };
     showErrorMessage();
-    
+    const disableButton = () => {
+        buttonEl.addEventListener('click', () => {
+            if( loginEl.value || passwordEl.value == ""){
+                buttonEl.disabled = true;
+                loginEl.classList.toggle('error-input');
+                passwordEl.classList.toggle('error-input');
+            } ;
+        });
+    }
+    disableButton()
 
+    const isItTryData = () => {
+        buttonEl.addEventListener('click', () => {
+            const formErrorMessage = document.createElement('span');
+            formErrorMessage.innerText = 'Try again';
+            formErrorMessage.classList.add('error');
+            if (loginEl.value == 'admin@domain.com' && passwordEl.value == 'password123') {
+                formEl.classList.toggle('invisible', 'form');
+                messageEl.classList.toggle('visible','success-message');
+            } else{
+                formEl.nextElementSibling.insertAdjacentElement('beforebegin', formErrorMessage);
+                loginEl.value = '';
+                passwordEl.value = '';
+            };
+        });
+    }
+    isItTryData()
 })();

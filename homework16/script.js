4b3b3f5e9e913ea6deecb67fe71540725522e1f3
calliopa <calliopa@centro.team>
2021-12-16 16:24:10 +0200

    progress

--- homework16/script.js
+++ homework16/script.js
@@ -1,22 +1,18 @@
 ( () =>{
-    const tableEL = document.getElementById('table');
-    const trEl = document.createElement('tr');
-    const tdEl = document.createElement('td');
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://reqres.in/api/users', false)
     xhr.send();
 
     let {data : users} = JSON.parse(xhr.response)
-    tableEL.append(trEl);
-    trEl.append(tdEl);
+
     
     function showUsers(el) {
-        el.forEach(i => {
-            for (let value in i) {
-                tdEl.append(i[value]);
-                // console.log(i[value])
-            }
+        el.forEach(element => {
+            Object.keys(element).forEach( i =>{
+                console.log(i)
+            })
+
         });
-    }
+}
     showUsers(users);
 })();

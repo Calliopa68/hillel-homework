3446a3fd0196a6ec562f1056dad3a97c28bbf2f2
calliopa <calliopa@centro.team>
2021-12-22 22:30:46 +0200

    progress

--- homework16/script.js
+++ homework16/script.js
@@ -1,18 +1,22 @@
-( () =>{
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://reqres.in/api/users', false)
-    xhr.send();
+// ( () =>{
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'https://reqres.in/api/users', false)
+//     xhr.send();
 
-    let {data : users} = JSON.parse(xhr.response)
+//     let {data : users} = JSON.parse(xhr.response)
 
     
-    function showUsers(el) {
-        el.forEach(element => {
-            Object.keys(element).forEach( i =>{
-                console.log(i)
-            })
+//     function showUsers(el) {
+//         el.forEach(element => {
+//             Object.keys(element).forEach( i =>{
+//                 console.log(i)
+//             })
 
-        });
-}
-    showUsers(users);
-})();
+//         });
+// }
+//     showUsers(users);
+// })();
+
+const sentence = "PriveT kkkfl dlAlL"
+
+ console.log(sentence.toLowerCase(0).split('').join(','))
